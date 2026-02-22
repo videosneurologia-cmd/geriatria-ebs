b1c4e6719c7c4390a2f97a1fdda5a240d125729d
videosneurologia-cmd <videosneurologia@gmail.com>
2026-02-22 16:14:47 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'geriatria-v1';
+const assets = [
+  'index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
